d7cac53bfab8031434211a1e1b0d91bcd8d0d6b8
mounir ben ammar <mounir.ben.ammar2@gmail.com>
2024-10-07 17:48:32 +0100

    loop

--- Week_2/Day_1/Practice/Fizzbuzz/Loop_Challenges/Loop_Challenges.js
+++ Week_2/Day_1/Practice/Fizzbuzz/Loop_Challenges/Loop_Challenges.js
@@ -7,3 +7,46 @@ function LoopOdd () {
     }
     
     LoopOdd ()
+
+
+    function DecreasingMultiples () {
+        for ( var i = 100; i>=0; i--)
+            if (i % 3 ===0){
+                console.log (i)
+            }
+        
+        }
+        
+        DecreasingMultiples ()
+
+
+        
+            
+function PrintSequence () {
+    for (i=4;i>=-3.5;i-=1.5)
+            console.log (i);
+}
+
+PrintSequence ()
+
+
+
+
+
+var sum = 0
+for (var i=1; i<=100; i++){
+
+    sum = sum+i}
+
+        console.log (sum);
+
+
+
+
+
+var multiple = 1
+for (var i=1; i<=12; i++){
+
+    multiple = multiple*i}
+
+        console.log (multiple);
